62a9c97e6735b9eafed26c8884d403c2661de263
Luc de wit <luc.dewit@hotmail.nl>
2020-03-20 20:07:12 +0100

    started type identifier

--- components/typeIdentifier/typeIdentifier.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by lucde on 20-3-2020.
-//
-
-#ifndef ALPHA_TYPEIDENTIFIER_H
-#define ALPHA_TYPEIDENTIFIER_H
-
-#endif //ALPHA_TYPEIDENTIFIER_H
---
+++ components/typeIdentifier/typeIdentifier.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <vector>
+
+#include "../tokenSys/tokenSys.hpp"
+
+bool isNumber(std::string);
+void typeIdentifier(std::vector<Token>&);
