abf9d16edc3be086d3bb02d2736a145094ca3b96
Luc de wit <luc.dewit@hotmail.nl>
2020-04-01 16:26:18 +0200

    fixed marker bug

--- components/interpreter/interpreter.cpp
+++ components/interpreter/interpreter.cpp
@@ -2,11 +2,11 @@
 
 void interpret(std::vector<Token> tokens, std::map<std::string, std::function<void(ARGUMENTS)>> functions, std::map<std::string, int> markerMemory){
     unsigned int lineNumber = 0;
+    printToken(tokens[94]);
     while (lineNumber < tokens.size()-1){
         Token command = tokens[lineNumber];
         lineNumber += 1+command.arguments.size();
-
-        std::cout << command.value << "\n";
+        //std::cout << command.value << "\n";
         functions[command.value](command.arguments, lineNumber, markerMemory);
     }
     std::exit(0);
--- components/markerExtracter/markerExtracter.cpp
+++ components/markerExtracter/markerExtracter.cpp
@@ -7,18 +7,20 @@ std::map<std::string, int> extractMarkers(std::vector<Token>& tokens)
     std::vector<int> toBeRemoved;
 
     int i = 0;
+    int offset = 0;
     for (const auto& t : tokens)
     {
         if (t.type == alph_marker)
         {
             std::cout << "[debug] found marker named " << t.value << ", must be deleted at index " << i << "\n";
-            markerMemory[t.value.substr(1, t.value.size()-1)] = i;
+            markerMemory[t.value.substr(1, t.value.size()-1)] = i-offset;
             toBeRemoved.push_back(i);
+            offset++;
         }
         i++;
     }
 
-    int offset = 0;
+    offset = 0;
     for (const auto& j : toBeRemoved){
 
         tokens.erase(tokens.begin()+(j-offset));
